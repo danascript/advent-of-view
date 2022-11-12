1cca524452aac04a2f63656cfcf31ac802b85ddc
danascript <22255733+danascript@users.noreply.github.com>
2022-11-12 14:06:45 +0100

    Solved challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,vue}'],
-  theme: {
-    colors: {
-      green: '#42b883',
-      'gray-dark': '#0f172a',
-    },
-  },
+  theme: {},
 }
